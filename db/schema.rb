d11acc06db5779fa7401b0ee2ad3d2036b6b16f4
kbjude <kbjude@gmail.com>
2020-05-30 01:05:39 +0300

    sign anf sign out

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_132653) do
+ActiveRecord::Schema.define(version: 2020_05_29_182258) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
