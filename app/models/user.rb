ef73d28a9f7dc071c1c0a713b72890c512a1eea0
kbjude <kbjude@gmail.com>
2020-07-06 12:14:33 +0300

    Need for a complete revert

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-         has_many :tweeets
+
+  has_many :tweeets
 end
